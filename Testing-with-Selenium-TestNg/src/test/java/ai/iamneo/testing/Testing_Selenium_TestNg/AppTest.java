c16042cc47d68c68c88c0f22c9c84518b7e3c706
Iamneo <techadmin@iamneo.ai>
2023-09-06 09:07:31 +0000

    Backup Commit

---
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -0,0 +1,17 @@
+package ai.iamneo.testing.Testing_Selenium_TestNg;
+
+import org.testng.annotations.Test;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class AppTest {
+
+
+
+}
